28076c72e48b4942aa158e2325fdb82f2df6581a
Pablo Mateos <pmateos805@gmail.com>
2022-06-03 20:33:09 +0200

    feat: first post route and test link component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
-export default function myFirstPost() {
-    return <h1>Hi my first post here</h1>
+import Link from "next/link"
+
+export default function firstPost() {
+    return (
+        <>
+            <h1>Hi my first post here</h1>
+            <h4>
+                <Link href='/'>
+                    <a>Back to home</a>
+                </Link>
+            </h4>
+        </>
+    )
 }
