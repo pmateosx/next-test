0bc8cd521a57896bbeb8d80fb5bc8c8f8454833a
Pablo Mateos <pmateos805@gmail.com>
2022-06-08 16:23:02 +0200

    feat: profile done

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
+import Head from "next/head"
 import Link from "next/link"
 
 export default function firstPost() {
     return (
         <>
+        <Head>
+            <title> First post · by ffff</title>
+        </Head>
             <h1>Hi my first post here</h1>
             <h4>
                 <Link href='/'>
